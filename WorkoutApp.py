4a33b352e6a48bd292c140f28f9e727c883ad7b6
tobias <toudiczwa@gmail.com>
2017-12-26 18:07:59 +0100

    Updated welcome screen and workout display and functionality

--- WorkoutApp.py
+++ WorkoutApp.py
@@ -24,7 +24,7 @@ def printer(timer=0, ex_reps ="", ex_name="", next_ex_reps="", next_ex_name="", 
             if key_pressed == 32:  # space
                 break
             if key_pressed == 27:  # escape
-                exit()
+                MainMenu()
             time.sleep(0.4)
             os.system('cls')
     else:
@@ -54,7 +54,12 @@ def printer(timer=0, ex_reps ="", ex_name="", next_ex_reps="", next_ex_name="", 
             else:
                 printASCII("  " + str(next_ex_reps) + " x")
             printASCII("  " + next_ex_name)
-            if  x <= 3:
+            if  x == 0:
+                winsound.Beep(3500, 200)
+                time.sleep(0.1)
+                winsound.Beep(3500, 200)
+                time.sleep(0.5)
+            elif  x <= 4:
                 winsound.Beep(2500, 500)
                 time.sleep(0.5)
             else:
@@ -64,81 +69,144 @@ def printer(timer=0, ex_reps ="", ex_name="", next_ex_reps="", next_ex_name="", 
             if key_pressed == 32:   #space
                 break
             if key_pressed== 27:   #escape
-                exit()
-            if key_pressed == 112:   #P
+                MainMenu()
+            # if key_pressed == 112:   #P
+            if key_pressed != 0:  # P
                 pause = True
             while pause == True:
                 key_pressed = kbfunc()
-                # if key_pressed == 112:  # P
+                if key_pressed == 27:  # escape
+                    MainMenu()
+                if key_pressed == 224:  # special (two character are returned)
+                    kbfunc()
+                    pause = False
                 if key_pressed != 0:  # P
                     pause = False
-                if key_pressed == 27:  # escape
-                    exit()
                 print("\n\n")
                 printASCII("   PAUSE")
                 time.sleep(0.5)
                 os.system('cls')
 
 def Duration(start, end):
+    """ return string which contains time duration between start and end time (from int(time.time())
+    return time format: MM:SS
+    return example: "01:34"
+    """
     duration = end - start
     minutes = str((duration % 3600) // 60).zfill(2)
     seconds = str(duration % 60).zfill(2)
     return "  " + minutes + ":" + seconds
 
 def doWorkout(workout):
-    nr_of_exercises = workout.nrOfExs()
-    timeStart = int(time.time())
+    nr_of_exercises = workout.GetNrOfExcercises()
+    nr_of_sets = workout.GetNumberOfSets()
+    start_time = int(time.time())
     printer(timer=workout.break_excercise,
             ex_reps=" ",
             ex_name="get ready",
-            next_ex_reps=workout.exReps(0, 0),
-            next_ex_name=workout.exName(0, 0) )
-    for serie in range(workout.nr_of_sets+1):
-        for ex_nr in range(nr_of_exercises):
-            if ex_nr == (nr_of_exercises-1) and serie ==(workout.nr_of_sets):    # lest, finish fater that
-                printer(timer =         workout.exTime(ex_nr, serie),
-                        ex_reps =       workout.exReps(ex_nr, serie),
-                        ex_name =       workout.exName(ex_nr, serie),
-                        next_ex_reps =  "",
-                        next_ex_name =  "the end",
-                        info="  END OF SET NR:{0}, NEXT:  ".format(serie+1))
-                timeStop = int(time.time())
-                printer(timer =         0,
-                        ex_reps =       "",
-                        ex_name =       "  GOOD JOB",
-                        next_ex_reps =  "TIME: " + Duration(timeStart, timeStop),
-                        next_ex_name =  "",
-                        info="  YOUR WORKOUT TIME:  ")
+            next_ex_reps=workout.GetRepetitions(0, 0),
+            next_ex_name=workout.GetExcerciseName(0, 0),
+            info="  SET {0}/{1} WILL START , NEXT:  ".format(1, nr_of_sets))
+    if workout.structure == "circuit":
+        for serie in range(nr_of_sets):
+            for ex_nr in range(nr_of_exercises):
+                if ex_nr == (nr_of_exercises-1) and serie ==(nr_of_sets-1):    # lest, finish fater that
+                    printer(timer =         workout.GetTime(ex_nr, serie),
+                            ex_reps =       workout.GetRepetitions(ex_nr, serie),
+                            ex_name =       workout.GetExcerciseName(ex_nr, serie),
+                            next_ex_reps =  "",
+                            next_ex_name =  "the end",
+                            info = "  SET {0}/{1}, EX({2}/{3}, NEXT:  ".format(serie + 1, nr_of_sets, ex_nr + 1, nr_of_exercises))
+                    stop_time = int(time.time())
+                    printer(timer =         0,
+                            ex_reps =       "",
+                            ex_name =       "  GOOD JOB",
+                            next_ex_reps =  "TIME: " + Duration(start_time, stop_time),
+                            next_ex_name =  "",
+                            info="  YOU'VE JUST FINISHED " + workout.name + " :  ")
 
-            elif ex_nr == (nr_of_exercises-1):                          # last in set
-                printer(timer =         workout.exTime(ex_nr, serie),
-                        ex_reps =       workout.exReps(ex_nr, serie),
-                        ex_name =       workout.exName(ex_nr, serie),
-                        next_ex_reps =  "",
-                        next_ex_name =  "long break",
-                        info="  END OF SET NR:{0}, NEXT:  ".format(serie+1))
-                printer(timer =         workout.break_set,
-                        ex_reps =       " ",
-                        ex_name =       "long break",
-                        next_ex_reps =  workout.exReps(0, serie+1),
-                        next_ex_name =  workout.exName(0, serie+1),
-                        info="  SET NR:{0} WILL START, NEXT:  ".format(serie+2))
+                elif ex_nr == (nr_of_exercises-1):                          # last in set
+                    printer(timer =         workout.GetTime(ex_nr, serie),
+                            ex_reps =       workout.GetRepetitions(ex_nr, serie),
+                            ex_name =       workout.GetExcerciseName(ex_nr, serie),
+                            next_ex_reps =  "",
+                            next_ex_name =  "long break",
+                            info="  SET {0}/{1}, EX({2}/{3}, NEXT:  ".format(serie + 1, nr_of_sets, ex_nr + 1, nr_of_exercises))
+                    printer(timer =         workout.break_set,
+                            ex_reps =       " ",
+                            ex_name =       "long break",
+                            next_ex_reps =  workout.GetRepetitions(0, serie+1),
+                            next_ex_name =  workout.GetExcerciseName(0, serie+1),
+                            info="  SET {0}/{1} EX({2}/{3}, WILL START , NEXT:  ".format(serie+2, nr_of_sets, ex_nr+1, nr_of_exercises))
 
-            else:                                                       # rest
-                printer(timer =         workout.exTime(ex_nr, serie),
-                        ex_reps =       workout.exReps(ex_nr, serie),
-                        ex_name =       workout.exName(ex_nr, serie),
-                        next_ex_reps =  "",
-                        next_ex_name =  "break",
-                        info="  SET NR:{0}, NEXT:  ".format(serie+1))
-                printer(timer =         workout.break_excercise,
-                        ex_reps =       " ",
-                        ex_name =       "break",
-                        next_ex_reps =  workout.exReps(ex_nr+1, serie),
-                        next_ex_name =  workout.exName(ex_nr+1, serie),
-                        info="  SET NR:{0}, NEXT:  ".format(serie+1))
+                else:                                                       # rest
+                    printer(timer =         workout.GetTime(ex_nr, serie),
+                            ex_reps =       workout.GetRepetitions(ex_nr, serie),
+                            ex_name =       workout.GetExcerciseName(ex_nr, serie),
+                            next_ex_reps =  "",
+                            next_ex_name =  "break",
+                            info="  SET {0}/{1}, EX({2}/{3}, NEXT:  ".format(serie+1, nr_of_sets, ex_nr+1, nr_of_exercises))
+                    printer(timer =         workout.break_excercise,
+                            ex_reps =       " ",
+                            ex_name =       "break",
+                            next_ex_reps =  workout.GetRepetitions(ex_nr+1, serie),
+                            next_ex_name =  workout.GetExcerciseName(ex_nr+1, serie),
+                            info="  SET {0}/{1}, EX({2}/{3}, NEXT:  ".format(serie + 1, nr_of_sets, ex_nr+1, nr_of_exercises))
+
+##################################################################################################################################################
+##                CLASSIC STruCt
+##################################################################################################################################################
+    # if workout.structure == "classic":
+    #     for serie in range(nr_of_sets):
+    #         for ex_nr in range(nr_of_exercises):
+    #             if ex_nr == (nr_of_exercises-1) and serie ==(nr_of_sets-1):    # lest, finish fater that
+    #                 printer(timer =         workout.GetTime(ex_nr, serie),
+    #                         ex_reps =       workout.GetRepetitions(ex_nr, serie),
+    #                         ex_name =       workout.GetExcerciseName(ex_nr, serie),
+    #                         next_ex_reps =  "",
+    #                         next_ex_name =  "the end",
+    #                         info="  END OF SET {0}/{1} , NEXT:  ".format(serie+1, nr_of_sets))
+    #                 stop_time = int(time.time())
+    #                 printer(timer =         0,
+    #                         ex_reps =       "",
+    #                         ex_name =       "  GOOD JOB",
+    #                         next_ex_reps =  "TIME: " + Duration(start_time, stop_time),
+    #                         next_ex_name =  "",
+    #                         info="  YOU'VE JUST FINISHED " + workout.name + " :  ")
+    #
+    #             elif ex_nr == (nr_of_exercises-1):                          # last in set
+    #                 printer(timer =         workout.GetTime(ex_nr, serie),
+    #                         ex_reps =       workout.GetRepetitions(ex_nr, serie),
+    #                         ex_name =       workout.GetExcerciseName(ex_nr, serie),
+    #                         next_ex_reps =  "",
+    #                         next_ex_name =  "long break",
+    #                         info="  END OF SET {0}/{1} , NEXT:  ".format(serie+1, nr_of_sets))
+    #                 printer(timer =         workout.break_set,
+    #                         ex_reps =       " ",
+    #                         ex_name =       "long break",
+    #                         next_ex_reps =  workout.GetRepetitions(0, serie+1),
+    #                         next_ex_name =  workout.GetExcerciseName(0, serie+1),
+    #                         info="  SET {0}/{1} WILL START , NEXT:  ".format(serie+2, nr_of_sets))
+    #
+    #             else:                                                       # rest
+    #                 printer(timer =         workout.GetTime(ex_nr, serie),
+    #                         ex_reps =       workout.GetRepetitions(ex_nr, serie),
+    #                         ex_name =       workout.GetExcerciseName(ex_nr, serie),
+    #                         next_ex_reps =  "",
+    #                         next_ex_name =  "break",
+    #                         info="  SET {0}/{1}, EX({2}/{3}, NEXT:  ".format(serie+1, nr_of_sets, ex_nr, nr_of_exercises))
+    #                 printer(timer =         workout.break_excercise,
+    #                         ex_reps =       " ",
+    #                         ex_name =       "break",
+    #                         next_ex_reps =  workout.GetRepetitions(ex_nr+1, serie),
+    #                         next_ex_name =  workout.GetExcerciseName(ex_nr+1, serie),
+    #                         info="  SET {0}/{1} , NEXT:  ".format(serie+1, nr_of_sets))
+
 
 def kbfunc():
+    """ return Unicode code of pressed button
+    return 0 if no button pressed
+    """
     x = msvcrt.kbhit()
     if x:
         ret = ord(msvcrt.getch())
@@ -146,17 +214,44 @@ def kbfunc():
         ret = 0
     return ret
 
-while True:
-    print("\n")
-    printASCII("  WORKOUT APP")
-    print("\n \t {0:<15} - start workout, jump to break/excercise".format("SPACE"))
-    print("\t {0:<15} - pause".format("P"))
-    print("\t {0:<15} - exit \n".format("ESCAPE"))
-    oneFiveMin.printWorkout()
+def KeyPress():
     key = kbfunc()
-    if key in [13, 32]:
-        doWorkout(oneFiveMin)
-    elif key == 27:
+    if key in [13, 32]:  # enter, space
+        current_workout = Workout.GetWorkout()
+        doWorkout(current_workout)
+    elif key == 27:  # escape
         exit()
-    time.sleep(1)
-    os.system('cls')
+    elif key == 224:  # special key prefix
+        key_2 = kbfunc()
+        # time.sleep(0.0)
+        if key_2 == 77:  # right
+            Workout.ChangeSelectionRight()
+        if key_2 == 75:  # left
+            Workout.ChangeSelectionLeft()
+    return key
+
+def MainMenu():
+
+    def PrintSelectionList():
+        selection_list = ["|__| "] * Workout.GetNumberOfWorkouts()
+        selection_list[Workout.GetPosition()] = "|##| "
+        selection = "".join(selection_list)
+        print("\t < {0:<15} > \n".format(selection))
+
+    while True:
+        print()
+        printASCII("  WORKOUT APP")
+        print("\n \t {0:<15} - start workout, jump to break/excercise".format("SPACE"))
+        print("\t {0:<15} - exit ".format("ESCAPE"))
+        print("\t {0:<15} - pause \n".format("P _any other ;)"))
+        PrintSelectionList()
+        Workout.GetWorkout().printWorkout()
+        # time.sleep(0.1)
+
+        while True:
+            if KeyPress() != 0:
+                break
+
+        os.system('cls')
+
+MainMenu()
--- training.py
+++ training.py
@@ -1,63 +1,191 @@
 class Workout(object):
-    """
-    workout class
+    """  workout class
+        name -   workout name,
+        plan -   list of  repetitions and time for each excercise and set
+
+                                      set   1                   2
+                           name             repetitions, time   repetitions, time
+                member [["jumpin jacks", [  [25,45],            [30, 60]]   ]
+
                plan[e] - consective excercise e block
                plan[e][0] - excercise e name
                plan[e][1] - excercise e matrix
                plan[e][1][s] - excercise e set s  list of blocks [reps, time]
                plan[e][1][s][0] - excercise e set s repetetions r
                plan[e][1][s][1] - excercise e set s time t
+
+        break_excercise -   break between excercises in set (s)
+        break_set -         break between sets (s)
     """
-    def __init__(self):
-        self.name = ""
-        self.plan = [   ["", [ [None, None], [None, None] ] ] ]
-        self.break_excercise =   10
-        self.break_set =         30
-        self.nr_of_sets = max( [len(x[1]) for x in self.plan] )
+    _registry = []  # list of all instances
+    _position = 0  # position(index) of selected workout in _registry
+
+    def __init__(self, name="", plan=None, break_excercise=10, break_set=30, structure="circuit"):
+        self._registry.append(self)
+        self.name = name
+        if plan == None:
+            plan = [["push ups", [[10, 60], [10, 60]]]]
+        self.plan = plan
+        self.break_excercise = break_excercise
+        self.break_set = break_set
+        self.structure = structure
+        self.nr_of_sets = max([len(x[1]) for x in self.plan])
         self.nr_of_exercises = len(self.plan)
-    def exName(self, ex_nr, ser):
+
+    def GetExcerciseName(self, ex_nr, ser):
         return self.plan[ex_nr][0]
-    def repsTime(self, ex_nr, set):
+
+    def GetRepetitionsTime(self, ex_nr, set):
         return self.plan[ex_nr][1][set]
-    def exReps(self, ex_nr, set):
+
+    def GetRepetitions(self, ex_nr, set):
         return self.plan[ex_nr][1][set][0]
-    def exTime(self, ex_nr, set):
+
+    def GetTime(self, ex_nr, set):
         return self.plan[ex_nr][1][set][1]
-    def exList(self):
+
+    def GetPlan(self):
         return [x[0] for x in self.plan]
-    def nrOfExs(self):
-        return len(self.plan)
-    def nrOfSets(self):
-        max([len(x[1]) for x in self.plan])
+
+    def GetNrOfExcercises(self):
+        self.nr_of_exercises = len(self.plan)
+        return self.nr_of_exercises
+
+    def GetNumberOfSets(self):
+        self.nr_of_sets = max([len(x[1]) for x in self.plan])
+        return self.nr_of_sets
+
+    @classmethod
+    def GetListOfWorkouts(cls):
+        return list(cls._registry)
+
+    @classmethod
+    def GetNumberOfWorkouts(cls):
+        return len(cls._registry)
+
+    @classmethod
+    def GetPosition(cls):
+        return cls._position
+
+    @classmethod
+    def ChangeSelectionRight(cls):
+        # switch workout right
+        if cls._position == cls.GetNumberOfWorkouts() - 1:
+            cls._position = 0
+        else:
+            cls._position += 1
+
+    @classmethod
+    def ChangeSelectionLeft(cls):
+        # switch workout left
+        if cls._position == 0:
+            cls._position = cls.GetNumberOfWorkouts() - 1
+        else:
+            cls._position -= 1
+
+    @classmethod
+    def GetWorkout(cls):
+        """ return workout object from list of all workouts (_registry)
+        index is based on the _position class atribute
+        """
+        return cls._registry[cls._position]
+
     def printWorkout(self):
-        print("\t%-25s %s" %("workout name:", self.name))
-        print("\t%-25s %s" %("snumber of sets:", self.nr_of_sets+1))
-        print("\t%-25s %s" %("number of excerices:", self.nrOfExs()))
-        print("\t%-25s %s%s" %("break between excercises:", self.break_excercise, "s"))
-        print("\t%-25s %s%s" %("break between sets:", self.break_set, "s"))
+        """  print the informations about workout (times, repetitions, plan) """
+        print("\t%-25s %s" % ("workout name:", self.name))
+        print("\t%-25s %s" % ("structure:", self.structure))
+        print("\t%-25s %s" % ("max. number of sets:", self.GetNumberOfSets()))
+        print("\t%-25s %s" % ("number of excerices:", self.GetNrOfExcercises()))
+        print("\t%-25s %s%s" % ("break between excercises:", self.break_excercise, "s"))
+        print("\t%-25s %s%s" % ("break between sets:", self.break_set, "s"))
         # nr_of_sets = mself.nr_of_sets
-        print("\n\t%-30s" %"set: " , end=' ')
-        for x in range(self.nr_of_sets+1):
-            print("%-16s" %(x+1), end=" ")
+        print("\n\t%-30s" % "set: ", end=' ')
+        for x in range(self.nr_of_sets):
+            print("%-16s" % (x + 1), end=" ")
         print("\n\t%-25s" % ("repetitions - time"), end=' ')
-        for x in range(self.nr_of_sets+1):
+        for x in range(self.nr_of_sets):
             print("%-6s %-8s " % ("reps", "time"), end=" ")
         for row in self.plan:
-            print("\n\t%-25s" %row[0], end=' ')
+            print("\n\t%-25s" % row[0], end=' ')
             for block in row[1]:
-                if block[0] == -1: rep = "hold"
-                else: rep = str(block[0])+"x"
-                time = str(block[1])+"s"
+                if block[0] == -1:
+                    rep = "hold"
+                else:
+                    rep = str(block[0]) + "x"
+                time = str(block[1]) + "s"
                 print("%-6s %-8s " % (rep, time), end=" ")
         print("\n")
 
-oneFiveMin = Workout()
-oneFiveMin.name = "15 minutes workout"
-oneFiveMin.plan= [      ["jumpin jacks",   [   [25, 45], [30, 60], [35, 60]]   ],\
-                        ["squats",         [   [25, 60], [30, 75], [35, 75]]   ],\
-                        ["plank",          [   [-1, 25], [-1, 30], [-1, 35]]   ],\
-                        ["lunges",         [   [20, 60], [24, 75], [28, 75]]   ],\
-                        ["push ups",       [   [10, 30], [12, 45], [15, 45]]   ],\
-                        ["crunches",       [   [25, 75], [30, 90], [35, 90]]   ] ]
-oneFiveMin.break_excercise =   10
-oneFiveMin.break_set =         30
+
+oneFiveMin = Workout(name="15 minutes workout",
+                     plan=[["jumpin jacks", [[25, 45], [30, 60], [35, 60]]], \
+                           ["squats", [[25, 60], [30, 75], [35, 75]]], \
+                           ["plank", [[-1, 25], [-1, 30], [-1, 35]]], \
+                           ["lunges", [[20, 60], [24, 75], [28, 75]]], \
+                           ["push ups", [[10, 30], [12, 45], [15, 45]]], \
+                           ["crunches", [[25, 75], [30, 90], [35, 90]]]],
+                     break_excercise=10,
+                     break_set=30,
+                     structure="circuit")
+
+bodyTooningWorkout = Workout(name="body tooning workout",
+                             plan=[["squats", [[25, 45]]], \
+                                   ["push ups", [[30, 120]]], \
+                                   ["burpees", [[30, 120]]], \
+                                   ["crunches", [[30, 120]]], \
+                                   ["dips", [[30, 120]]]],
+                             break_excercise=60,
+                             break_set=0,
+                             structure="circuit")
+
+rollWithIt = Workout(name="roll with it",
+                     plan=[["roll ins", [[10, 60], [10, 60], [10, 60]]], \
+                           ["push ups", [[10, 60], [10, 60], [10, 60]]], \
+                           ["bridges", [[10, 60], [10, 60], [10, 60]]], \
+                           ["crunches", [[20, 60], [20, 60], [20, 60]]], \
+                           ["side crunches", [[20, 60], [20, 60], [20, 60]]], \
+                           ["back ext.", [[20, 60], [20, 60], [20, 60]]]],
+                     break_excercise=30,
+                     break_set=120,
+                     structure="circuit")
+
+betterSleep = Workout(name="better sleep yoga",
+                      plan=[["hero pose", [[-1, 20]]], \
+                            ["child pose", [[-1, 20]]], \
+                            ["upward dog", [[-1, 20]]], \
+                            ["camel pose", [[-1, 20]]], \
+                            ["butterfly fold", [[-1, 20]]], \
+                            ["supine twis", [[-1, 20]]], \
+                            ["bridge", [[-1, 20]]], \
+                            ["knee-to-chest", [[-1, 20]]], \
+                            ["corpse pose", [[-1, 20]]]],
+                      break_excercise=10,
+                      break_set=10,
+                      structure="circuit")
+
+fullBodyStretch = Workout(name="full body stretch",
+                          plan=[["neck s.", [[-1, 40]]], \
+                                ["shoulder s.", [[-1, 40]]], \
+                                ["tricep s.", [[-1, 40]]], \
+                                ["pelvic s.", [[-1, 40]]], \
+                                ["quad s.", [[-1, 40]]], \
+                                ["forward bend", [[-1, 40]]]],
+                          break_excercise=10,
+                          break_set=10,
+                          structure="circuit")
+
+dayBreak = Workout(name="daybreak",
+                   plan=[["crapula shr.", [[10, 40], [10, 40], [10, 40], [10, 40]]], \
+                         ["r. angels", [[10, 40], [10, 40], [10, 40], [10, 40]]], \
+                         ["extensions", [[10, 40], [10, 40], [10, 40], [10, 40]]], \
+                         ["r. prone", [[10, 40], [10, 40], [10, 40], [10, 40]]], \
+                         ["quad s.", [[10, 40], [10, 40], [10, 40], [10, 40]]], \
+                         ["forward bend", [[10, 40], [10, 40], [10, 40], [10, 40]]]],
+                   break_excercise=20,
+                   break_set=120,
+                   structure="classic")
+
+if __name__ == "__main__":
+    for workout_object in Workout._registry:
+        workout_object.printWorkout()
+        print()
