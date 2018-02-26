f968b70cbf74ddc0a169fb7ed5c566d3a3098b0b
tobias <tobiaszkedzierski@gmail.com>
2018-02-26 19:34:20 +0100

    upgrade font scaling

--- gui_version/files/view.py
+++ gui_version/files/view.py
@@ -6,28 +6,41 @@ from datetime import timedelta
 
 class Timer:
     def __init__(self, master):
-        # dimensions
+        # general properties
+        self.bgcolor = "black"
+
+        # ring properties
+        self.ring_color = "red4"
         self.initial_size = 300
         self.ring_margin = 20
         self.ring_width = 20
-        self.dimensions = {"timer_ring_margin": 70}
+
+        # text properties
+        self.central_text_size = 30
+        self.central_text_color = "white"
+        self.central_text_font = {"font": "Helvetica", "style": "bold"}
+
 
         self.master = master
         self.master.title("Workout App")
         self.master.geometry("{0}x{0}".format(self.initial_size))
         self.master.minsize(300, 300)
-        self.master.configure(background="black")
+        self.master.configure(background=self.bgcolor)
 
         self.canvas = Canvas(self.master)
         self.canvas.pack()
         self.canvas.config(width=self.initial_size, height=self.initial_size, background="black", highlightthickness=0)
+        self.ring_arc = self.canvas.create_arc(*self.ring_outter_coords)
+        self.canvas.itemconfigure(self.ring_arc, start = 90, extent=270, fill=self.ring_color, width=0)
+        self.ring_inner_oval = self.canvas.create_oval(*self.ring_inner_coords, fill=self.bgcolor)
 
-        self.arc = self.canvas.create_arc(self.ring_margin, self.ring_margin,
-                                          self.canvas_size-self.ring_margin, self.canvas_size-self.ring_margin)
-        self.canvas.itemconfigure(self.arc, start = 90, extent=270, fill="red4", width=0)
 
-        self.oval = self.canvas.create_oval(30, 30, 270, 270, fill="black")
-        self.center_text = self.canvas.create_text(150, 150, text="center_text", font = ("Verdanaaaa", 30, "bold"), fill="white")
+        self.central_text = self.canvas.create_text(*self.central_text_coords, text="central_text",
+                                                    font=(self.central_text_font.get("font"),
+                                                          self.central_text_size,
+                                                          self.central_text_font.get("style")),
+                                                    fill=self.central_text_color)
+
         self.text_action = self.canvas.create_text(150, 180, text="text_action", font=("Courier", 15, "bold"), fill="gray48")
         self.text_shut_time = self.canvas.create_text(150, 120, text="text_shut_time", font=("Courier", 15), fill="gray48")
 
@@ -46,6 +59,37 @@ class Timer:
     def ring_margin(self, x):
         self.__ring_margin = self.calc_dim(x)
 
+    @property
+    def ring_outter_coords(self):
+        return (self.ring_margin, self.ring_margin,
+                self.canvas_size-self.ring_margin, self.canvas_size-self.ring_margin)
+
+    @property
+    def ring_width(self):
+        return self.get_dim(self.__ring_width)
+
+    @ring_width.setter
+    def ring_width(self, x):
+        self.__ring_width = self.calc_dim(x)
+
+    @property
+    def ring_inner_coords(self):
+        return (self.ring_width + self.ring_margin, self.ring_width + self.ring_margin,
+                self.canvas_size - self.ring_width - self.ring_margin,
+                self.canvas_size - self.ring_width - self.ring_margin)
+    
+    @property
+    def central_text_size(self):
+        return self.get_dim(self.__central_text_size)
+    
+    @central_text_size.setter
+    def central_text_size(self, x):
+        self.__central_text_size = self.calc_dim(x)
+
+    @property
+    def central_text_coords(self):
+        return int(self.canvas_size/2), int(self.canvas_size/2)
+
     def calc_dim(self, x):
         return x/self.canvas_size
 
@@ -53,17 +97,18 @@ class Timer:
         return int(x*self.canvas_size)
 
     def scale_timer(self):
-        print("test_variable:", self.ring_margin)
+        print("test_variable:", self.central_text_font, self.central_text_size)
         self.canvas.config(width=self.canvas_size, height=self.canvas_size)
-        self.canvas.itemconfigure(self.center_text, font=("Helveticaaa", int(self.canvas_size * 0.1), "bold"))
+        self.canvas.itemconfigure(self.central_text, font=(self.central_text_font.get("font"),
+                                                           self.central_text_size,
+                                                           self.central_text_font.get("style")))
+
         self.canvas.itemconfigure(self.text_action, font=("Courier", int(self.canvas_size*0.05), "bold"))
         self.canvas.itemconfigure(self.text_shut_time, font=("Courier", int(self.canvas_size*0.05)))
 
-        self.canvas.coords(self.arc, self.ring_margin, self.ring_margin,
-                           self.canvas_size-self.ring_margin, self.canvas_size-self.ring_margin)
+        self.canvas.coords(self.ring_arc, *self.ring_outter_coords)
 
-        self.canvas.coords(self.oval, self.canvas_size*0.1, self.canvas_size*.1, self.canvas_size-(self.canvas_size*0.1), self.canvas_size-(self.canvas_size*0.1))
-        self.canvas.coords(self.center_text, self.canvas_size * 0.5, self.canvas_size * 0.5)
+        self.canvas.coords(self.ring_inner_oval, *self.ring_inner_coords)
+        self.canvas.coords(self.central_text, *self.central_text_coords)
         self.canvas.coords(self.text_action, self.canvas_size * 0.5, self.canvas_size * 0.6)
         self.canvas.coords(self.text_shut_time, self.canvas_size * 0.5, self.canvas_size * 0.4)
-
