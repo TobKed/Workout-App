4ed40daf5f228262a6faa628c0e8cddeec5d01ce
tobias <tobiaszkedzierski@gmail.com>
2018-02-26 21:11:14 +0100

    add texts

--- gui_version/files/view.py
+++ gui_version/files/view.py
@@ -18,8 +18,28 @@ class Timer:
         # text properties
         self.central_text_size = 30
         self.central_text_color = "white"
-        self.central_text_font = {"font": "Helvetica", "style": "bold"}
-
+        self.central_text_font = {"font": "Helvetica", "style": "normal"}
+        
+        self.text_plus_1_size = 15
+        self.text_plus_1_color = "white"
+        self.text_plus_1_font = {"font": "Helvetica", "style": "normal"}
+        self.text_plus_1_position = 0.63
+        
+        self.text_plus_2_size = 10
+        self.text_plus_2_color = "white"
+        self.text_plus_2_font = {"font": "Helvetica", "style": "bold"}
+        self.text_plus_2_position = 0.75
+        
+        self.text_minus_1_size = 15
+        self.text_minus_1_color = "white"
+        self.text_minus_1_font = {"font": "Helvetica", "style": "normal"}
+        self.text_minus_1_position = 0.37
+        
+        self.text_minus_2_size = 10
+        self.text_minus_2_color = "white"
+        self.text_minus_2_font = {"font": "Helvetica", "style": "normal"}
+        self.text_minus_2_position = 0.3
+        
 
         self.master = master
         self.master.title("Workout App")
@@ -40,9 +60,36 @@ class Timer:
                                                           self.central_text_size,
                                                           self.central_text_font.get("style")),
                                                     fill=self.central_text_color)
+        
+        self.text_plus_1 = self.canvas.create_text(*self.text_plus_1_coords, text="text_plus_1",
+                                                    font=(self.text_plus_1_font.get("font"),
+                                                          self.text_plus_1_size,
+                                                          self.text_plus_1_font.get("style")),
+                                                    fill=self.text_plus_1_color)
+        
+        self.text_plus_2 = self.canvas.create_text(*self.text_plus_2_coords, text="text_plus_2",
+                                                    font=(self.text_plus_2_font.get("font"),
+                                                          self.text_plus_2_size,
+                                                          self.text_plus_2_font.get("style")),
+                                                    fill=self.text_plus_2_color)
+        
+        self.text_minus_1 = self.canvas.create_text(*self.text_minus_1_coords, text="text_minus_1",
+                                                    font=(self.text_minus_1_font.get("font"),
+                                                          self.text_minus_1_size,
+                                                          self.text_minus_1_font.get("style")),
+                                                    fill=self.text_minus_1_color)
+        
+        self.text_minus_2 = self.canvas.create_text(*self.text_minus_2_coords, text="text_minus_2",
+                                                    font=(self.text_minus_2_font.get("font"),
+                                                          self.text_minus_2_size,
+                                                          self.text_minus_2_font.get("style")),
+                                                    fill=self.text_minus_2_color)
 
-        self.text_action = self.canvas.create_text(150, 180, text="text_action", font=("Courier", 15, "bold"), fill="gray48")
-        self.text_shut_time = self.canvas.create_text(150, 120, text="text_shut_time", font=("Courier", 15), fill="gray48")
+    def calc_dim(self, x):
+        return x/self.canvas_size
+
+    def get_dim(self, x):
+        return int(x*self.canvas_size)
 
     @property
     def canvas_size(self):
@@ -78,6 +125,9 @@ class Timer:
                 self.canvas_size - self.ring_width - self.ring_margin,
                 self.canvas_size - self.ring_width - self.ring_margin)
     
+    ##############################
+    #   TEXT DECORATORS START    #
+    ##############################
     @property
     def central_text_size(self):
         return self.get_dim(self.__central_text_size)
@@ -85,30 +135,93 @@ class Timer:
     @central_text_size.setter
     def central_text_size(self, x):
         self.__central_text_size = self.calc_dim(x)
-
+        
     @property
     def central_text_coords(self):
         return int(self.canvas_size/2), int(self.canvas_size/2)
+    
+    # text_plus_1
+    @property
+    def text_plus_1_size(self):
+        return self.get_dim(self.__text_plus_1_size)
 
-    def calc_dim(self, x):
-        return x/self.canvas_size
+    @text_plus_1_size.setter
+    def text_plus_1_size(self, x):
+        self.__text_plus_1_size = self.calc_dim(x)
 
-    def get_dim(self, x):
-        return int(x*self.canvas_size)
+    @property
+    def text_plus_1_coords(self):
+        return int(self.canvas_size / 2), int(self.canvas_size * (1-self.text_plus_1_position))
+    
+    # text_plus_2
+    @property
+    def text_plus_2_size(self):
+        return self.get_dim(self.__text_plus_2_size)
+
+    @text_plus_2_size.setter
+    def text_plus_2_size(self, x):
+        self.__text_plus_2_size = self.calc_dim(x)
+
+    @property
+    def text_plus_2_coords(self):
+        return int(self.canvas_size / 2), int(self.canvas_size * (1-self.text_plus_2_position))
+    
+    # text_minus_1
+    @property
+    def text_minus_1_size(self):
+        return self.get_dim(self.__text_minus_1_size)
+
+    @text_minus_1_size.setter
+    def text_minus_1_size(self, x):
+        self.__text_minus_1_size = self.calc_dim(x)
+
+    @property
+    def text_minus_1_coords(self):
+        return int(self.canvas_size / 2), int(self.canvas_size * (1-self.text_minus_1_position))
+    
+    # text_minus_2
+    @property
+    def text_minus_2_size(self):
+        return self.get_dim(self.__text_minus_2_size)
+
+    @text_minus_2_size.setter
+    def text_minus_2_size(self, x):
+        self.__text_minus_2_size = self.calc_dim(x)
+
+    @property
+    def text_minus_2_coords(self):
+        return int(self.canvas_size / 2), int(self.canvas_size * (1-self.text_minus_2_position))
+
+    ##############################
+    #    TEXT DECORATORS END     #
+    ##############################
 
     def scale_timer(self):
-        print("test_variable:", self.central_text_font, self.central_text_size)
         self.canvas.config(width=self.canvas_size, height=self.canvas_size)
         self.canvas.itemconfigure(self.central_text, font=(self.central_text_font.get("font"),
                                                            self.central_text_size,
                                                            self.central_text_font.get("style")))
-
-        self.canvas.itemconfigure(self.text_action, font=("Courier", int(self.canvas_size*0.05), "bold"))
-        self.canvas.itemconfigure(self.text_shut_time, font=("Courier", int(self.canvas_size*0.05)))
-
+        
+        self.canvas.itemconfigure(self.text_plus_1, font=(self.text_plus_1_font.get("font"),
+                                                           self.text_plus_1_size,
+                                                           self.text_plus_1_font.get("style")))
+        
+        self.canvas.itemconfigure(self.text_plus_2, font=(self.text_plus_2_font.get("font"),
+                                                           self.text_plus_2_size,
+                                                           self.text_plus_2_font.get("style")))
+        
+        self.canvas.itemconfigure(self.text_minus_1, font=(self.text_minus_1_font.get("font"),
+                                                           self.text_minus_1_size,
+                                                           self.text_minus_1_font.get("style")))
+        
+        self.canvas.itemconfigure(self.text_minus_2, font=(self.text_minus_2_font.get("font"),
+                                                           self.text_minus_2_size,
+                                                           self.text_minus_2_font.get("style")))
+        
         self.canvas.coords(self.ring_arc, *self.ring_outter_coords)
-
         self.canvas.coords(self.ring_inner_oval, *self.ring_inner_coords)
         self.canvas.coords(self.central_text, *self.central_text_coords)
-        self.canvas.coords(self.text_action, self.canvas_size * 0.5, self.canvas_size * 0.6)
-        self.canvas.coords(self.text_shut_time, self.canvas_size * 0.5, self.canvas_size * 0.4)
+        self.canvas.coords(self.text_plus_1, *self.text_plus_1_coords)
+        self.canvas.coords(self.text_plus_2, *self.text_plus_2_coords)
+        self.canvas.coords(self.text_minus_1, *self.text_minus_1_coords)
+        self.canvas.coords(self.text_minus_2, *self.text_minus_2_coords)
